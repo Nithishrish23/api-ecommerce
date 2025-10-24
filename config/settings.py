bc64c2deeb8edb303edd4c2984bd21209c09fbc6
NithishKumar <127183096+Nithishrish23@users.noreply.github.com>
2025-10-24 10:39:57 +0530

    Change database settings to use Supabase
    Updated database configuration for Supabase.

--- config/settings.py
+++ config/settings.py
@@ -3,10 +3,10 @@ class Config:
     SECRET_KEY = "pclinfo"
     MAX_CONTENT_LENGTH = 100 * 1024 * 1024
     UPLOAD_FOLDER= os.path.join(r'uploads')
-    DB_NAME = "ecom"
+    DB_NAME = "postgres"
     DB_USER = "postgres"
-    DB_PASSWORD = "1976"
-    DB_HOST = "localhost"
+    DB_PASSWORD = "CDfW6x7AI2yIm7tb"
+    DB_HOST = "db.nbchszbhkckcoyxguqvg.supabase.co"
     DB_PORT = "5432"
     # Optional Stripe configuration (set programmatically here or via environment variables)
     STRIPE_SECRET_KEY = None

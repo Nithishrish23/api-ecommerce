d927db256a6b00c727f300cb59217355248a17df
NithishKumar <127183096+Nithishrish23@users.noreply.github.com>
2025-11-01 16:15:16 +0530

    Update database configuration in settings.py

--- config/settings.py
+++ config/settings.py
@@ -3,10 +3,10 @@ class Config:
     SECRET_KEY = "pclinfo"
     MAX_CONTENT_LENGTH = 100 * 1024 * 1024
     UPLOAD_FOLDER= os.path.join(r'uploads')
-    DB_NAME = "ecom"
-    DB_USER = "postgres"
-    DB_PASSWORD = "CDfW6x7AI2yIm7tb"
-    DB_HOST = "db.nbchszbhkckcoyxguqvg.supabase.co"
+    DB_NAME = "ecom_bpdb"
+    DB_USER = "ecom_bpdb_user"
+    DB_PASSWORD = "Hz40FiLGSwYGYDL4JPbleUKXJB76hwEA"
+    DB_HOST = "dpg-d42u7bur433s73e11lo0-a"
     DB_PORT = "5432"
     # Optional Stripe configuration (set programmatically here or via environment variables)
     STRIPE_SECRET_KEY = None

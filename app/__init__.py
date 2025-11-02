6fc2607896a206faf787bdae6ca8eeabf182c33d
Nithishrish23 <nithishrish@yahoo.com>
2025-11-02 19:36:04 +0530

    Initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,12 @@ from flask import Flask
 from .routes import blueprint
 def create_app():
     app = Flask(__name__)
-    app.config['SESSION_TYPE'] = 'filesystem'
-    app.config['SESSION_COOKIE_SECURE'] = True  # Use True if running over HTTPS
-    app.config['SESSION_COOKIE_HTTPONLY'] = True
-    app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
+    # app.config['SESSION_TYPE'] = 'filesystem'
+    # app.config['SESSION_COOKIE_SECURE'] = True  # Use True if running over HTTPS
+    # app.config['SESSION_COOKIE_HTTPONLY'] = True
+    # app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
     app.config['SECRET_KEY'] = 'pclinfo'
-    app.config['SESSION_FILE_DIR']=r'flask_session'
+    # app.config['SESSION_FILE_DIR']=r'flask_session'
 
     # Session(app)
 
